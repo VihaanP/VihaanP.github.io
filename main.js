dd231f68d4feb3691f84d2b6518edbd8dfb620c9
VihaanP <66943639+VihaanP@users.noreply.github.com>
2021-04-08 22:34:05 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+let myImage = document.querySelector("img");
+
+myImage.onclick = function () {
+  mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/Re-zero.jpg") {
+    myImage.setAttribute("src", "images/Gang.jpg");
+  } else {
+    myImage.setAttribute("src", "images/Re-zero.jpg");
+  }
+};
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUsername() {
+  let myName = prompt("Please enter your name");
+  if (!myName) {
+    setUsername();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = "Re Zero arc 5 is a masterpiece," + myName;
+  }
+}
+if (!localStorage.getItem("name")) {
+  setUsername();
+} else {
+  let storedName = localStorage.getItem("name");
+  myHeading.textContent = "Re Zero arc 5 is a masterpiece," + storedName;
+}
+myButton.onclick = function () {
+  setUsername();
+};
